e0d671008d8af9843f05886e5c7193f4bd412acb
Farid Rn <faridv@gmail.com>
2015-04-18 18:17:00 +0430

    Login register modal

--- js/main.js
+++ js/main.js
@@ -7,12 +7,6 @@ $(function () {
     }).on('shown.bs.popover', function () {
 	$('#search-form-inner input:first').focus();
     });
-    
-    // User popover
-    $('.user [data-toggle=popover]').popover({
-	content: $("#user-form").html()
-	, html: true
-    });
 
     // Close popovers on focusout
     $(document).on('blur', '.popover', function () {
